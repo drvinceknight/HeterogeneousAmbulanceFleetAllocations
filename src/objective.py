81ffd16822279b37dccaba321ba6bab7de499594
Vince Knight <vincent.knight@gmail.com>
2023-06-05 15:54:52 +0100

    Add function to return objective function.
    This returns a single function g that will then be passed to the
    optimiser.
    
    This should be checked carefully.

--- src/objective.py
+++ src/objective.py
@@ -8,10 +8,108 @@ import numpy.typing as npt
 import numpy as np
 
 
+def get_objective_function(
+    pickup_locations: Iterable,
+    patient_type_partitions: Iterable,
+    survival_functions: Iterable[Callable],
+    primary_travel_times: npt.NDArray,
+    secondary_travel_times: npt.NDArray,
+    primary_vehicle_station_utilisation: Iterable,
+    secondary_vehicle_station_utilisation: Iterable,
+    vehicle_locations: Iterable,
+    is_station_closer_to_pickup_location: dict,
+    is_vehicle_type_closer_to_pickup_location: dict,
+    weights: dict,
+    demands: dict,
+) -> Callable:
+    """
+    Take all parameters of the given problem and generate a function of a single
+    variable (the allocations).
+    """
+    number_of_vehicle_locations = len(vehicle_locations)
+
+    def tilde_psi(primary_vehicle_allocation, secondary_vehicle_allocation, patient_type, pickup_location, ambulance_station):
+        """
+        Return tilde_psi
+        """
+        return get_multiple_vehicle_patient_survival(
+            patient_type=patient_type,
+            pickup_location=pickup_location,
+            ambulance_station=ambulance_station,
+            survival_functions=survival_functions,
+            primary_travel_times=primary_travel_times,
+            secondary_travel_times=secondary_travel_times,
+            primary_vehicle_station_utilisation=primary_vehicle_station_utilisation,
+            secondary_vehicle_station_utilisation=secondary_vehicle_station_utilisation,
+            vehicle_locations=vehicle_locations,
+            primary_vehicle_allocation=primary_vehicle_allocation,
+            secondary_vehicle_allocation=secondary_vehicle_allocation,
+            is_station_closer_to_pickup_location=is_station_closer_to_pickup_location,
+            is_vehicle_type_closer_to_pickup_location=is_vehicle_type_closer_to_pickup_location,
+        )
+
+    def psi(primary_vehicle_allocation, secondary_vehicle_station_utilisation, patient_type, pickup_location, ambulance_station):
+        """
+        Return psi
+        """
+        return get_single_vehicle_patient_survival(
+            patient_type=patient_type,
+            pickup_location=pickup_location,
+            ambulance_station=ambulance_station,
+            survival_functions=survival_functions,
+            travel_times=primary_travel_times,
+            station_utilisation=primary_vehicle_station_utilisation,
+            vehicle_locations=vehicle_locations,
+            vehicle_allocation=primary_vehicle_allocation,
+            is_station_closer_to_pickup_location=is_station_closer_to_pickup_location,
+        )
+
+    def g(allocation: npt.NDArray) -> float:
+        f"""
+        The objective function for the problem defined by the following
+        parameters:
+
+        - pickup_locations: {pickup_locations}
+        - patient_type_partitions: {patient_type_partitions}
+        - survival_functions: {survival_functions}
+        - primary_travel_times: {primary_travel_times}
+        - secondary_travel_times: {secondary_travel_times}
+        - primary_vehicle_station_utilisation: {primary_vehicle_station_utilisation}
+        - secondary_vehicle_station_utilisation: {secondary_vehicle_station_utilisation}
+        - vehicle_locations: {vehicle_locations}
+        - is_station_closer_to_pickup_location: {is_station_closer_to_pickup_location}
+        - is_vehicle_type_closer_to_pickup_location: {is_vehicle_type_closer_to_pickup_location}
+        - weights: {weights}
+        - demands: {demands}
+
+        It takes `allocation` as a single numpy array. The first half
+        corresponds to the location of the primary vehicles and the second half
+        to the location of the secondary vehicles.
+        """
+        primary_vehicle_allocation = allocation[:number_of_vehicle_locations]
+        secondary_vehicle_allocation = allocation[number_of_vehicle_locations:]
+        return sum(
+            get_survival(
+                pickup_locations=pickup_locations,
+                vehicle_locations=vehicle_locations,
+                primary_vehicle_allocation=primary_vehicle_allocation,
+                secondary_vehicle_allocation=secondary_vehicle_allocation,
+                patient_types=patient_types,
+                weights=weights,
+                demands=demands,
+                psi=psi,
+            )
+            for patient_types, psi in zip(patient_type_partitions, (psi, tilde_psi))
+        )
+    return g
+
+
 def get_survival(
     pickup_locations: Iterable,
     vehicle_locations: Iterable,
     patient_types: Iterable,
+    primary_vehicle_allocation: npt.NDArray,
+    secondary_vehicle_allocation: npt.NDArray,
     weights: dict,
     demands: dict,
     psi: Callable,
@@ -22,7 +120,7 @@ def get_survival(
     return sum(
         weights[patient_type]
         * demands[(patient_type, pickup_location)]
-        * psi(patient_type, pickup_location, ambulance_station)
+        * psi(primary_vehicle_allocation, secondary_vehicle_allocation, patient_type, pickup_location, ambulance_station)
         for patient_type in patient_types
         for ambulance_station in vehicle_locations
         for pickup_location in pickup_locations
--- src/test_objectives.py
+++ src/test_objectives.py
@@ -1,4 +1,5 @@
 import numpy as np
+import types
 
 import objective
 
@@ -70,7 +71,9 @@ def test_get_multiple_vehicle_patient_survival():
     ambulance_station = 2
     survival_functions = [lambda x: np.exp(-x)]
     primary_travel_times = np.array(((0, 3.3, 4.2, 3), (1, 0, 1, 2), (2, 3, 0, 2)))
-    secondary_travel_times = np.array(((0.1, 3.2, 4.1, 2), (1, 0, 1, 2), (2, 3.1, 0, 1.9)))
+    secondary_travel_times = np.array(
+        ((0.1, 3.2, 4.1, 2), (1, 0, 1, 2), (2, 3.1, 0, 1.9))
+    )
     primary_vehicle_station_utilisation = np.array((0.2, 0.4, 0.5))
     secondary_vehicle_station_utilisation = np.array((0.3, 0.2, 0.55))
     vehicle_locations = (0, 1, 2)
@@ -168,3 +171,147 @@ def test_get_multiple_vehicle_patient_survival():
         is_vehicle_type_closer_to_pickup_location=is_vehicle_type_closer_to_pickup_location,
     )
     assert np.isclose(output, 0.00557483879)
+
+
+def test_get_objective_function():
+    pickup_locations = (0, 1, 2, 3)
+    patient_type_partitions = ((0,), (1, 2))
+    survival_functions = (
+            lambda x: np.exp(-x), 
+            lambda x: 1 if x < 8 else 0,
+            lambda x: 1 if x < 16 else 0,
+            )
+    primary_travel_times = np.array(((0, 3.3, 4.2, 3), (1, 0, 1, 2), (2, 3, 0, 2)))
+    secondary_travel_times = np.array(
+        ((0.1, 3.2, 4.1, 2), (1, 0, 1, 2), (2, 3.1, 0, 1.9))
+    )
+    primary_vehicle_station_utilisation = np.array((0.2, 0.4, 0.5))
+    secondary_vehicle_station_utilisation = np.array((0.3, 0.2, 0.55))
+    vehicle_locations = (0, 1, 2)
+    is_station_closer_to_pickup_location = {
+        (0, 0, 0): False,
+        (0, 0, 1): False,
+        (0, 0, 2): False,
+        (0, 0, 3): False,
+        (0, 1, 0): True,
+        (0, 1, 1): False,
+        (0, 1, 2): False,
+        (0, 1, 3): False,
+        (0, 2, 0): True,
+        (0, 2, 1): False,
+        (0, 2, 2): True,
+        (0, 2, 3): False,
+        (1, 0, 0): True,
+        (1, 0, 1): True,
+        (1, 0, 2): False,
+        (1, 0, 3): False,
+        (1, 1, 0): False,
+        (1, 1, 1): False,
+        (1, 1, 2): False,
+        (1, 1, 3): False,
+        (1, 2, 0): True,
+        (1, 2, 1): False,
+        (1, 2, 2): True,
+        (1, 2, 3): False,
+        (2, 0, 0): True,
+        (2, 0, 1): True,
+        (2, 0, 2): False,
+        (2, 0, 3): False,
+        (2, 1, 0): False,
+        (2, 1, 1): False,
+        (2, 1, 2): False,
+        (2, 1, 3): False,
+        (2, 2, 0): False,
+        (2, 2, 1): False,
+        (2, 2, 2): False,
+        (2, 2, 3): False,
+        (3, 0, 0): True,
+        (3, 0, 1): True,
+        (3, 0, 2): False,
+        (3, 0, 3): False,
+        (3, 1, 0): False,
+        (3, 1, 1): False,
+        (3, 1, 2): False,
+        (3, 1, 3): False,
+        (3, 2, 0): False,
+        (3, 2, 1): False,
+        (3, 2, 2): False,
+        (3, 2, 3): False,
+    }
+    is_vehicle_type_closer_to_pickup_location = {
+        (0, 0, 0): False,
+        (0, 0, 1): False,
+        (0, 0, 2): False,
+        (0, 0, 3): False,
+        (0, 1, 0): True,
+        (0, 1, 1): False,
+        (0, 1, 2): True,
+        (0, 1, 3): False,
+        (0, 2, 0): True,
+        (0, 2, 1): False,
+        (0, 2, 2): True,
+        (0, 2, 3): False,
+        (1, 0, 0): True,
+        (1, 0, 1): True,
+        (1, 0, 2): False,
+        (1, 0, 3): False,
+        (1, 1, 0): False,
+        (1, 1, 1): False,
+        (1, 1, 2): False,
+        (1, 1, 3): False,
+        (1, 2, 0): True,
+        (1, 2, 1): False,
+        (1, 2, 2): True,
+        (1, 2, 3): False,
+        (2, 0, 0): True,
+        (2, 0, 1): True,
+        (2, 0, 2): False,
+        (2, 0, 3): False,
+        (2, 1, 0): True,
+        (2, 1, 1): False,
+        (2, 1, 2): False,
+        (2, 1, 3): False,
+        (2, 2, 0): False,
+        (2, 2, 1): False,
+        (2, 2, 2): False,
+        (2, 2, 3): False,
+        (3, 0, 0): True,
+        (3, 0, 1): True,
+        (3, 0, 2): False,
+        (3, 0, 3): False,
+        (3, 1, 0): False,
+        (3, 1, 1): False,
+        (3, 1, 2): False,
+        (3, 1, 3): False,
+        (3, 2, 0): False,
+        (3, 2, 1): False,
+        (3, 2, 2): False,
+        (3, 2, 3): False,
+    }
+    weights = (.5, .3, .2)
+    demands = np.array(
+            (
+                ))
+
+    demands = np.array(((2, 2, 3, 3), (2, 0, 1, 2), (1, 1, 1, 1)))
+    g = objective.get_objective_function(
+        pickup_locations=pickup_locations,
+        patient_type_partitions=patient_type_partitions,
+        survival_functions=survival_functions,
+        primary_travel_times=primary_travel_times,
+        secondary_travel_times=secondary_travel_times,
+        primary_vehicle_station_utilisation=primary_vehicle_station_utilisation,
+        secondary_vehicle_station_utilisation=secondary_vehicle_station_utilisation,
+        vehicle_locations=vehicle_locations,
+        is_station_closer_to_pickup_location=is_station_closer_to_pickup_location,
+        is_vehicle_type_closer_to_pickup_location=is_vehicle_type_closer_to_pickup_location,
+        weights=weights,
+        demands=demands,
+    )
+    assert isinstance(g, types.FunctionType)
+    allocations = np.array((0, 0, 0, 0, 0, 0))
+    assert g(allocations) == 0.0
+    allocations = np.array((1, 0, 2, 0, 3, 0))
+    assert np.isclose(g(allocations), 2.620185796108)
+    allocations = np.array((1, 50, 2, 20, 3, 5))
+    assert np.isclose(g(allocations), 4.07839246016)
