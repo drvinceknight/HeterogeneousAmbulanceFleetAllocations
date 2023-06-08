fixes realistic scenario time units, uses utilisations obtained from simulation, compares to simulation results

src/test_realistic_scneario.py
@@ -4,22 +4,27 @@ import numpy as np
 
 import objective
 
+## Time units in minutes
 raw_travel_times = np.genfromtxt("./travel_times_matrix.csv", delimiter=",")
 beta = objective.get_beta(travel_times=raw_travel_times)
 primary_vehicle_travel_times = raw_travel_times / 0.75
-secondary_vehicle_travel_times = raw_travel_times / 125
-R = objective.get_R(primary_vehicle_travel_times=primary_vehicle_travel_times,
-        secondary_vehicle_travel_times=secondary_vehicle_travel_times)
+secondary_vehicle_travel_times = raw_travel_times / 1.215
+
+R = objective.get_R(
+    primary_vehicle_travel_times=primary_vehicle_travel_times,
+    secondary_vehicle_travel_times=secondary_vehicle_travel_times
+)
 survival_functions = (
-        lambda t:  1 / (1 + np.exp(0.26 + 0.139 * t)),
-        lambda t: t <= 15,
-        lambda t: t <= 60,
-        )
-demand_rates = np.genfromtxt("./demand.csv", delimiter=",")
+    lambda t:  1 / (1 + np.exp(0.26 + 0.139 * t)),
+    lambda t: t <= 15,
+    lambda t: t <= 60,
+)
+demand_rates = np.genfromtxt("./demand.csv", delimiter=",") / 1440
 vehicle_locations, pickup_locations = tuple(map(range, raw_travel_times.shape))
-patient_type_partitions = ((0,), (1, 2))
-primary_vehicle_station_utilisation = [.5 for _ in raw_travel_times]
-secondary_vehicle_station_utilisation = [.5 for _ in raw_travel_times]
+patient_type_partitions = ((1, 2), (0,))
+primary_vehicle_station_utilisation = np.genfromtxt('./primary_utilisations_75.csv', delimiter=',') # Directly from simulation
+secondary_vehicle_station_utilisation = np.genfromtxt('./secondary_utilisations_75.csv', delimiter=',') # Directly from simulation
+
 allocation = np.genfromtxt("./allocation_75.csv", delimiter=",")
 
 def test_paramaters():
@@ -54,4 +59,5 @@ def test_objective_function():
     )
     assert isinstance(g, types.FunctionType)
     assert g([0 for _ in range(67 * 2)]) == 0
-    assert np.isclose(g(allocation), 131.21336710313716)
+    objective_in_days = g(allocation) * 1440
+    assert np.isclose(objective_in_days, 138.87802089723246) # Simulation says it should be 145.8548968064855 (survived patients per day on average)