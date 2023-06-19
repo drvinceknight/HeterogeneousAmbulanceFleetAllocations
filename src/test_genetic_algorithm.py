e656b4d0b4c51d12ca3ad8f3e097cfb27edfa205
Geraint Palmer <palmer.geraint@googlemail.com>
2023-06-19 15:07:39 +0100

    remove possibility of replacing vehicle type with another when mutating

--- src/test_genetic_algorithm.py
+++ src/test_genetic_algorithm.py
@@ -24,49 +24,6 @@ def test_move_vehicle_of_same_type():
     assert np.array_equal(secondary_allocation, resulting_secondary_allocation)
     assert np.array_equal(resulting_primary_allocation, np.array([0, 1, 4, 2]))
 
-
-def test_switch_primary_to_secondary():
-    primary_allocation = np.array([0, 1, 5, 1])
-    secondary_allocation = np.array([3, 9, 0, 0])
-    max_allocation = 5
-
-    np.random.seed(1)
-    (
-        resulting_primary_allocation,
-        resulting_secondary_allocation,
-    ) = genetic.switch_primary_to_secondary(
-        primary_allocation=primary_allocation,
-        secondary_allocation=secondary_allocation,
-        max_allocation=max_allocation,
-    )
-
-    assert sum(primary_allocation) - 1 == sum(resulting_primary_allocation)
-    assert sum(secondary_allocation) + 3 == sum(resulting_secondary_allocation)
-    assert np.array_equal(resulting_secondary_allocation, np.array([5, 9, 1, 0]))
-    assert np.array_equal(resulting_primary_allocation, np.array([0, 1, 4, 1]))
-
-
-def test_switch_secondary_to_primary():
-    primary_allocation = np.array([0, 1, 5, 1])
-    secondary_allocation = np.array([3, 9, 0, 0])
-    max_allocation = 5
-
-    np.random.seed(1)
-    (
-        resulting_primary_allocation,
-        resulting_secondary_allocation,
-    ) = genetic.switch_secondary_to_primary(
-        primary_allocation=primary_allocation,
-        secondary_allocation=secondary_allocation,
-        max_allocation=max_allocation,
-    )
-
-    assert sum(primary_allocation) + 1 == sum(resulting_primary_allocation)
-    assert sum(secondary_allocation) - 3 == sum(resulting_secondary_allocation)
-    assert np.array_equal(resulting_secondary_allocation, np.array([2, 7, 0, 0]))
-    assert np.array_equal(resulting_primary_allocation, np.array([1, 1, 5, 1]))
-
-
 def test_mutate_with_seed_0():
     primary_allocation = np.array([0, 1, 5, 1])
     secondary_allocation = np.array([3, 9, 0, 0])
@@ -76,7 +33,8 @@ def test_mutate_with_seed_0():
     resulting_primary_allocation, resulting_secondary_allocation = genetic.mutate(
         primary_allocation=primary_allocation,
         secondary_allocation=secondary_allocation,
-        max_allocation=max_allocation,
+        max_primary_allocation=max_allocation,
+        max_secondary_allocation=max_allocation,
     )
 
     assert sum(primary_allocation) + (sum(secondary_allocation) / 3) == sum(
@@ -95,7 +53,8 @@ def test_mutate_with_seed_1():
     resulting_primary_allocation, resulting_secondary_allocation = genetic.mutate(
         primary_allocation=primary_allocation,
         secondary_allocation=secondary_allocation,
-        max_allocation=max_allocation,
+        max_primary_allocation=max_allocation,
+        max_secondary_allocation=max_allocation,
     )
 
     assert sum(primary_allocation) + (sum(secondary_allocation) / 3) == sum(
@@ -103,44 +62,6 @@ def test_mutate_with_seed_1():
     ) + (sum(resulting_secondary_allocation) / 3)
     assert np.array_equal(resulting_secondary_allocation, np.array([3, 8, 0, 1]))
     assert np.array_equal(resulting_primary_allocation, np.array([0, 1, 5, 1]))
-
-
-def test_mutate_with_seed_3():
-    primary_allocation = np.array([0, 1, 5, 1])
-    secondary_allocation = np.array([3, 9, 0, 0])
-    max_allocation = 5
-
-    np.random.seed(3)
-    resulting_primary_allocation, resulting_secondary_allocation = genetic.mutate(
-        primary_allocation=primary_allocation,
-        secondary_allocation=secondary_allocation,
-        max_allocation=max_allocation,
-    )
-
-    assert sum(primary_allocation) + (sum(secondary_allocation) / 3) == sum(
-        resulting_primary_allocation
-    ) + (sum(resulting_secondary_allocation) / 3)
-    assert np.array_equal(resulting_secondary_allocation, np.array([5, 9, 1, 0]))
-    assert np.array_equal(resulting_primary_allocation, np.array([0, 0, 5, 1]))
-
-
-def test_mutate_with_seed_5():
-    primary_allocation = np.array([0, 1, 5, 1])
-    secondary_allocation = np.array([3, 9, 0, 0])
-    max_allocation = 5
-
-    np.random.seed(5)
-    resulting_primary_allocation, resulting_secondary_allocation = genetic.mutate(
-        primary_allocation=primary_allocation,
-        secondary_allocation=secondary_allocation,
-        max_allocation=max_allocation,
-    )
-
-    assert sum(primary_allocation) + (sum(secondary_allocation) / 3) == sum(
-        resulting_primary_allocation
-    ) + (sum(resulting_secondary_allocation) / 3)
-    assert np.array_equal(resulting_secondary_allocation, np.array([2, 7, 0, 0]))
-    assert np.array_equal(resulting_primary_allocation, np.array([0, 1, 5, 2]))
 
 
 def test_create_initial_population():
