54e866b7b1614e625447ef8459196e2d71d3a1fd
Vince Knight <vincent.knight@gmail.com>
2023-06-26 16:20:04 +0100

    Write objective function (#2)
    * Remove DS_Store files.
    
    * Write single vehicle patient survival function.
    
    * Add basic CI.
    
    * Add a requirements.txt file
    
    * Move requirements.txt file.
    
    * Add two vehicle type survival calculation.
    
    * Add black to CI.
    
    * Add function to return objective function.
    
    This returns a single function g that will then be passed to the
    optimiser.
    
    This should be checked carefully.
    
    * Format code.
    
    * Add tests with realistic scenario.
    
    * fixes realistic scenario (#4)
    
    * fixes realistic scenario time units, uses utilisations obtained from simulation, compares to simulation results
    
    * run black on all files
    
    * rerun black with latest black version 23.3.0
    
    * Add more realistic scenarios (#5)
    
    * add more test cases for realistic scenarios and compare them to simulated results
    
    * reformat files with black
    
    * Write mutation functions for the genetic algorithm (#6)
    
    * write mutate functions
    
    * reformat with black
    
    * Rewrite objective function using vectors (#7)
    
    * rewrite objective function as vectors
    
    * Fixes discrepencies between old and vectorised objectives:
      + old objective was not adding properly in psi_tilde
      + vecorised objective was not raising to a power correctly
      + Both used a wring definition of R
    
    * change realistic scenario to flood system more, to see more difference in objective values
    
    * reformat with black
    
    * Complete genetic algorithm (#9)
    
    * add function to create_initial_population
    
    * add function to rank a population according to the objective
    
    * remove possibility of replacing vehicle type with another when mutating
    
    * add optimisation algorithm
    
    * Make minor readability amendments
    
    * Fix the definition of Beta.
    
    This fixes the problem related to ties but also ensures that no computation of the station in question is carried.
    
    * Correct readability fix.
    
    * Add correct inequality to R calculation.
    
    If the times are equal: we want to consider the availability of a primary vehicle.
    
    * fix R and beta transpose bugs
    
    * reformat with black
    
    * test_objectives now tests for vectorised objective
    
    * remove old objective and rename objective_vectorised to objective
    
    * fix test checking that ranking population decreases the objective
    
    * reformat with black
    
    * add test for full optimisation
    
    * rename 'genetic.py' to 'optimisation.py'
    
    * bring back mutations swapping vehicle types; optimise function takes a mutation function
    
    * Add possibility to pass utilisation function.
    
    * Fix bug and update test value for new test.
    
    * Manual edits to rebase.
    
    * fixes after merge
    
    * Consider utilisations (#13)
    
    * add some standard utilisation functions
    
    * create ready made utilisation functions, pass kwargs to everything
    
    * update realistic comments, run black
    
    * combine primary and secondary utilisation functions
    
    * calculate utilisations by solving demand equations
    
    ---------
    
    Co-authored-by: Vince Knight <vincent.knight@gmail.com>
    
    ---------
    
    Co-authored-by: Geraint Palmer <palmer.geraint@googlemail.com>

---
+++ src/optimisation.py
@@ -0,0 +1,311 @@
+import numpy as np
+import objective
+import tqdm
+
+
+def move_vehicle_of_same_type(
+    allocation_for_moving, allocation_not_for_moving, max_allocation
+):
+    """
+    Randomly moves on primary from one primary location to another.
+    """
+    locations_with_vehicle = np.where(allocation_for_moving)[0]
+    locations_to_move_to = np.where(allocation_for_moving < max_allocation)[0]
+    from_location = np.random.choice(locations_with_vehicle)
+    is_not_from_location = locations_to_move_to != from_location
+    to_location = np.random.choice(
+        locations_to_move_to, p=is_not_from_location / np.sum(is_not_from_location)
+    )
+    new_allocation_for_moving = np.array(allocation_for_moving)
+    new_allocation_not_for_moving = np.array(allocation_not_for_moving)
+    new_allocation_for_moving[from_location] -= 1
+    new_allocation_for_moving[to_location] += 1
+    return new_allocation_for_moving, new_allocation_not_for_moving
+
+
+def switch_primary_to_secondary(
+    primary_allocation,
+    secondary_allocation,
+    max_allocation,
+    primary_to_secondary_ratio=3,
+):
+    """
+    Randomly remove a primary vehicle and create `primary_to_secondary_ratio` secondary vehicles.
+    """
+    locations_with_primary = np.where(primary_allocation)[0]
+    locations_to_move_to = np.where(secondary_allocation < max_allocation)[0]
+    from_location = np.random.choice(locations_with_primary)
+    to_locations = np.random.choice(
+        locations_to_move_to, size=primary_to_secondary_ratio
+    )
+    new_primary_allocation = np.array(primary_allocation)
+    new_secondary_allocation = np.array(secondary_allocation)
+    new_primary_allocation[from_location] -= 1
+    for to_loc in to_locations:
+        new_secondary_allocation[to_loc] += 1
+    return new_primary_allocation, new_secondary_allocation
+
+
+def switch_secondary_to_primary(
+    primary_allocation,
+    secondary_allocation,
+    max_allocation,
+    primary_to_secondary_ratio=3,
+):
+    """
+    Randomly add a primary vehicle and remove `primary_to_secondary_ratio` secondary vehicles.
+    """
+    locations_with_secondary = np.where(secondary_allocation)[0]
+    locations_to_move_to = np.where(primary_allocation < max_allocation)[0]
+    from_locations = np.random.choice(
+        locations_with_secondary, size=primary_to_secondary_ratio
+    )
+    to_location = np.random.choice(locations_to_move_to)
+    new_primary_allocation = np.array(primary_allocation)
+    new_secondary_allocation = np.array(secondary_allocation)
+    new_primary_allocation[to_location] += 1
+    for from_loc in from_locations:
+        new_secondary_allocation[from_loc] -= 1
+    return new_primary_allocation, new_secondary_allocation
+
+
+def mutate_full(
+    primary_allocation,
+    secondary_allocation,
+    max_primary,
+    max_secondary,
+    primary_to_secondary_ratio=3,
+):
+    number_primary_vehicles = sum(primary_allocation)
+    number_secondary_vehicles = sum(secondary_allocation)
+    possible_mutations = [
+        lambda x, y, max_primary, max_secondary: move_vehicle_of_same_type(
+            allocation_for_moving=x,
+            allocation_not_for_moving=y,
+            max_allocation=max_primary,
+        )
+    ]
+    if number_secondary_vehicles > 0:
+        possible_mutations.append(
+            lambda x, y, max_primary, max_secondary: move_vehicle_of_same_type(
+                allocation_for_moving=y,
+                allocation_not_for_moving=x,
+                max_allocation=max_secondary,
+            )[::-1]
+        )
+    if (
+        number_primary_vehicles * primary_to_secondary_ratio
+        > number_secondary_vehicles + primary_to_secondary_ratio
+    ):
+        possible_mutations.append(
+            lambda x, y, max_primary, max_secondary: switch_primary_to_secondary(
+                primary_allocation=x,
+                secondary_allocation=y,
+                max_allocation=max_secondary,
+            )
+        )
+    if number_secondary_vehicles > primary_to_secondary_ratio:
+        possible_mutations.append(
+            lambda x, y, max_primary, max_secondary: switch_secondary_to_primary(
+                primary_allocation=x, secondary_allocation=y, max_allocation=max_primary
+            )
+        )
+    mutation_function = np.random.choice(possible_mutations)
+    return mutation_function(
+        primary_allocation, secondary_allocation, max_primary, max_secondary
+    )
+
+
+def mutate_retain_vehicle_numbers(
+    primary_allocation,
+    secondary_allocation,
+    max_primary,
+    max_secondary,
+):
+    number_primary_vehicles = sum(primary_allocation)
+    number_secondary_vehicles = sum(secondary_allocation)
+    possible_mutations = [
+        lambda x, y, max_primary, max_secondary: move_vehicle_of_same_type(
+            allocation_for_moving=x,
+            allocation_not_for_moving=y,
+            max_allocation=max_primary,
+        )
+    ]
+    if number_secondary_vehicles > 0:
+        possible_mutations.append(
+            lambda x, y, max_primary, max_secondary: move_vehicle_of_same_type(
+                allocation_for_moving=y,
+                allocation_not_for_moving=x,
+                max_allocation=max_secondary,
+            )[::-1]
+        )
+    mutation_function = np.random.choice(possible_mutations)
+    return mutation_function(
+        primary_allocation, secondary_allocation, max_primary, max_secondary
+    )
+
+
+def create_initial_population(
+    number_of_locations,
+    number_of_primary_vehicles,
+    number_of_secondary_vehicles,
+    max_primary,
+    max_secondary,
+    population_size,
+):
+    """
+    Creates a (population_size, 2, number_of_locations) array of population_size allocations.
+    Each allocation is a (2, number_of_locations) array consisting of a primary allocation and a secondary allocation.
+    """
+    population = []
+    for entry in range(population_size):
+        # create primary allocation
+        primary_allocation = np.zeros(number_of_locations)
+        temp = np.random.choice(
+            np.arange(number_of_locations).repeat(max_primary),
+            number_of_primary_vehicles,
+            replace=False,
+        )
+        locs, numbs = np.unique(temp, return_counts=True)
+        primary_allocation[locs] += numbs
+
+        # create secondary allocation
+        secondary_allocation = np.zeros(number_of_locations)
+        temp = np.random.choice(
+            np.arange(number_of_locations).repeat(max_secondary),
+            number_of_secondary_vehicles,
+            replace=False,
+        )
+        locs, numbs = np.unique(temp, return_counts=True)
+        secondary_allocation[locs] += numbs
+
+        # add to population
+        population.append([primary_allocation, secondary_allocation])
+    return np.array(population)
+
+
+def rank_population(
+    population,
+    demand_rates,
+    primary_survivals,
+    secondary_survivals,
+    weights_single_vehicle,
+    weights_multiple_vehicles,
+    beta,
+    R,
+    vehicle_station_utilisation_function,
+    **kwargs,
+):
+    """
+    Ranks the population according to the objective function
+    """
+    objective_values = []
+    for allocation in population:
+        objective_values.append(
+            -objective.get_objective(
+                demand_rates=demand_rates,
+                primary_survivals=primary_survivals,
+                secondary_survivals=secondary_survivals,
+                weights_single_vehicle=weights_single_vehicle,
+                weights_multiple_vehicles=weights_multiple_vehicles,
+                beta=beta,
+                R=R,
+                vehicle_station_utilisation_function=vehicle_station_utilisation_function,
+                allocation_primary=allocation[0],
+                allocation_secondary=allocation[1],
+                **kwargs,
+            )
+        )
+    ordering = np.argsort(objective_values)
+    return np.array(population[ordering]), -np.array(objective_values)[ordering]
+
+
+def optimise(
+    number_of_locations,
+    number_of_primary_vehicles,
+    number_of_secondary_vehicles,
+    max_primary,
+    max_secondary,
+    population_size,
+    keep_size,
+    number_of_iterations,
+    mutation_function,
+    demand_rates,
+    primary_survivals,
+    secondary_survivals,
+    weights_single_vehicle,
+    weights_multiple_vehicles,
+    beta,
+    R,
+    vehicle_station_utilisation_function,
+    seed,
+    progress_bar=False,
+    **kwargs,
+):
+    """
+    Optimise
+    """
+    np.random.seed(seed)
+    objective_by_iteration = []
+    population = create_initial_population(
+        number_of_locations=number_of_locations,
+        number_of_primary_vehicles=number_of_primary_vehicles,
+        number_of_secondary_vehicles=number_of_secondary_vehicles,
+        max_primary=max_primary,
+        max_secondary=max_secondary,
+        population_size=population_size,
+    )
+
+    new_pop_size = population_size - keep_size
+
+    if progress_bar:
+        iterations = tqdm.tqdm(range(number_of_iterations))
+    else:
+        iterations = range(number_of_iterations)
+    for iteration in iterations:
+        ranked_population, objective_values = rank_population(
+            population=population,
+            demand_rates=demand_rates,
+            primary_survivals=primary_survivals,
+            secondary_survivals=secondary_survivals,
+            weights_single_vehicle=weights_single_vehicle,
+            weights_multiple_vehicles=weights_multiple_vehicles,
+            beta=beta,
+            R=R,
+            vehicle_station_utilisation_function=vehicle_station_utilisation_function,
+            **kwargs,
+        )
+        objective_by_iteration.append(objective_values)
+        kept_population = ranked_population[:keep_size]
+        new_population = []
+        for new_solution in range(new_pop_size):
+            solution_to_mutate = kept_population[np.random.choice(range(keep_size))]
+            mutated_solution = mutation_function(
+                primary_allocation=solution_to_mutate[0],
+                secondary_allocation=solution_to_mutate[1],
+                max_primary=max_primary,
+                max_secondary=max_secondary,
+            )
+            new_population.append(mutated_solution)
+        population = np.vstack([kept_population, np.array(new_population)])
+
+    ranked_population, objective_values = rank_population(
+        population=population,
+        demand_rates=demand_rates,
+        primary_survivals=primary_survivals,
+        secondary_survivals=secondary_survivals,
+        weights_single_vehicle=weights_single_vehicle,
+        weights_multiple_vehicles=weights_multiple_vehicles,
+        beta=beta,
+        R=R,
+        vehicle_station_utilisation_function=vehicle_station_utilisation_function,
+        **kwargs,
+    )
+
+    best_primary_population, best_secondary_population = ranked_population[0]
+
+    return (
+        best_primary_population,
+        best_secondary_population,
+        np.array(objective_by_iteration),
+    )
---
+++ src/test_objectives.py
@@ -0,0 +1,500 @@
+import numpy as np
+import types
+import objective
+import utilisation
+
+
+def test_get_beta():
+    travel_times = np.array(
+        [[0, 5, 10, 15, 20], [5, 0, 5, 10, 15], [10, 5, 0, 5, 10], [15, 10, 5, 0, 5]]
+    )
+    beta = objective.get_beta(travel_times)
+    expected_beta = np.array(
+        [
+            [
+                [0.0, 1.0, 1.0, 1.0],
+                [0.0, 0.0, 1.0, 1.0],
+                [0.0, 0.0, 0.0, 1.0],
+                [0.0, 0.0, 0.0, 0.0],
+            ],
+            [
+                [0.0, 0.0, 1.0, 1.0],
+                [1.0, 0.0, 1.0, 1.0],
+                [1.0, 0.0, 0.0, 1.0],
+                [0.0, 0.0, 0.0, 0.0],
+            ],
+            [
+                [0.0, 0.0, 0.0, 0.0],
+                [1.0, 0.0, 0.0, 1.0],
+                [1.0, 1.0, 0.0, 1.0],
+                [1.0, 1.0, 0.0, 0.0],
+            ],
+            [
+                [0.0, 0.0, 0.0, 0.0],
+                [1.0, 0.0, 0.0, 0.0],
+                [1.0, 1.0, 0.0, 0.0],
+                [1.0, 1.0, 1.0, 0.0],
+            ],
+            [
+                [0.0, 0.0, 0.0, 0.0],
+                [1.0, 0.0, 0.0, 0.0],
+                [1.0, 1.0, 0.0, 0.0],
+                [1.0, 1.0, 1.0, 0.0],
+            ],
+        ]
+    )
+    assert np.allclose(beta, expected_beta)
+
+
+def test_get_R():
+    primary_travel_times = np.array(
+        [[0, 5, 10, 15, 20], [5, 0, 5, 10, 15], [10, 5, 0, 5, 10], [15, 10, 5, 0, 5]]
+    )
+    secondary_travel_times = 0.7 * primary_travel_times
+    R = objective.get_R(primary_travel_times, secondary_travel_times)
+    expected_R = np.array(
+        [
+            [
+                [1.0, 1.0, 1.0, 1.0],
+                [0.0, 0.0, 1.0, 1.0],
+                [0.0, 0.0, 0.0, 1.0],
+                [0.0, 0.0, 0.0, 0.0],
+            ],
+            [
+                [0.0, 0.0, 0.0, 1.0],
+                [1.0, 1.0, 1.0, 1.0],
+                [0.0, 0.0, 0.0, 1.0],
+                [0.0, 0.0, 0.0, 0.0],
+            ],
+            [
+                [0.0, 0.0, 0.0, 0.0],
+                [1.0, 0.0, 0.0, 0.0],
+                [1.0, 1.0, 1.0, 1.0],
+                [1.0, 0.0, 0.0, 0.0],
+            ],
+            [
+                [0.0, 0.0, 0.0, 0.0],
+                [1.0, 0.0, 0.0, 0.0],
+                [1.0, 1.0, 0.0, 0.0],
+                [1.0, 1.0, 1.0, 1.0],
+            ],
+            [
+                [0.0, 0.0, 0.0, 0.0],
+                [0.0, 0.0, 0.0, 0.0],
+                [1.0, 1.0, 0.0, 0.0],
+                [1.0, 1.0, 1.0, 0.0],
+            ],
+        ]
+    )
+    assert np.allclose(R, expected_R)
+
+
+def test_relationship_between_beta_and_R():
+    travel_times = np.array(
+        [[0, 5, 10, 15, 20], [5, 0, 5, 10, 15], [10, 5, 0, 5, 10], [15, 10, 5, 0, 5]]
+    )
+    R = objective.get_R(travel_times, travel_times)
+    beta = objective.get_beta(travel_times)
+    expected_difference_is_identity = np.array(
+        [
+            [
+                [1.0, 0.0, 0.0, 0.0],
+                [0.0, 1.0, 0.0, 0.0],
+                [0.0, 0.0, 1.0, 0.0],
+                [0.0, 0.0, 0.0, 1.0],
+            ],
+            [
+                [1.0, 0.0, 0.0, 0.0],
+                [0.0, 1.0, 0.0, 0.0],
+                [0.0, 0.0, 1.0, 0.0],
+                [0.0, 0.0, 0.0, 1.0],
+            ],
+            [
+                [1.0, 0.0, 0.0, 0.0],
+                [0.0, 1.0, 0.0, 0.0],
+                [0.0, 0.0, 1.0, 0.0],
+                [0.0, 0.0, 0.0, 1.0],
+            ],
+            [
+                [1.0, 0.0, 0.0, 0.0],
+                [0.0, 1.0, 0.0, 0.0],
+                [0.0, 0.0, 1.0, 0.0],
+                [0.0, 0.0, 0.0, 1.0],
+            ],
+            [
+                [1.0, 0.0, 0.0, 0.0],
+                [0.0, 1.0, 0.0, 0.0],
+                [0.0, 0.0, 1.0, 0.0],
+                [0.0, 0.0, 0.0, 1.0],
+            ],
+        ]
+    )
+    np.allclose(R - beta, expected_difference_is_identity)
+
+
+def test_get_survival_vectors():
+    primary_travel_times = np.array(
+        [[0, 5, 10, 15, 20], [5, 0, 5, 10, 15], [10, 5, 0, 5, 10], [15, 10, 5, 0, 5]]
+    )
+    secondary_travel_times = primary_travel_times * 0.7
+    survival_functions = (
+        lambda t: np.ones(t.shape),
+        lambda t: np.heaviside(4 - t, 1),
+        lambda t: np.heaviside(14 - t, 1),
+    )
+
+    expected_primary_survivals = np.array(
+        [
+            [
+                [1.0, 1.0, 1.0, 1.0],
+                [1.0, 1.0, 1.0, 1.0],
+                [1.0, 1.0, 1.0, 1.0],
+                [1.0, 1.0, 1.0, 1.0],
+                [1.0, 1.0, 1.0, 1.0],
+            ],
+            [
+                [1.0, 0.0, 0.0, 0.0],
+                [0.0, 1.0, 0.0, 0.0],
+                [0.0, 0.0, 1.0, 0.0],
+                [0.0, 0.0, 0.0, 1.0],
+                [0.0, 0.0, 0.0, 0.0],
+            ],
+            [
+                [1.0, 1.0, 1.0, 0.0],
+                [1.0, 1.0, 1.0, 1.0],
+                [1.0, 1.0, 1.0, 1.0],
+                [0.0, 1.0, 1.0, 1.0],
+                [0.0, 0.0, 1.0, 1.0],
+            ],
+        ]
+    )
+    expected_secondary_survivals = np.array(
+        [
+            [
+                [1.0, 1.0, 1.0, 1.0],
+                [1.0, 1.0, 1.0, 1.0],
+                [1.0, 1.0, 1.0, 1.0],
+                [1.0, 1.0, 1.0, 1.0],
+                [1.0, 1.0, 1.0, 1.0],
+            ],
+            [
+                [1.0, 1.0, 0.0, 0.0],
+                [1.0, 1.0, 1.0, 0.0],
+                [0.0, 1.0, 1.0, 1.0],
+                [0.0, 0.0, 1.0, 1.0],
+                [0.0, 0.0, 0.0, 1.0],
+            ],
+            [
+                [1.0, 1.0, 1.0, 1.0],
+                [1.0, 1.0, 1.0, 1.0],
+                [1.0, 1.0, 1.0, 1.0],
+                [1.0, 1.0, 1.0, 1.0],
+                [1.0, 1.0, 1.0, 1.0],
+            ],
+        ]
+    )
+    primary_survivals, secondary_survivals = objective.get_survival_time_vectors(
+        survival_functions, primary_travel_times, secondary_travel_times
+    )
+    assert np.allclose(primary_survivals, expected_primary_survivals)
+    assert np.allclose(secondary_survivals, expected_secondary_survivals)
+
+
+def test_get_not_busy_vector():
+    allocation_1 = [0, 0, 0, 0]
+    allocation_2 = [0, 1, 1, 1]
+    allocation_3 = [1, 2, 3, 4]
+    utilisations = [0.2, 0.5, 0.7, 1.0]
+    not_busy_1 = objective.get_is_not_busy_vector(utilisations, allocation_1)
+    not_busy_2 = objective.get_is_not_busy_vector(utilisations, allocation_2)
+    not_busy_3 = objective.get_is_not_busy_vector(utilisations, allocation_3)
+    expected_not_busy_1 = np.array(
+        [1 - u**a for u, a in zip(utilisations, allocation_1)]
+    )
+    expected_not_busy_2 = np.array(
+        [1 - u**a for u, a in zip(utilisations, allocation_2)]
+    )
+    expected_not_busy_3 = np.array(
+        [1 - u**a for u, a in zip(utilisations, allocation_3)]
+    )
+    assert np.allclose(not_busy_1, expected_not_busy_1)
+    assert np.allclose(not_busy_2, expected_not_busy_2)
+    assert np.allclose(not_busy_3, expected_not_busy_3)
+
+
+def test_get_all_same_closer_busy_vector():
+    allocation_1 = [0, 0, 0, 0]
+    allocation_2 = [0, 1, 1, 1]
+    allocation_3 = [1, 2, 3, 4]
+    utilisations = [0.2, 0.5, 0.7, 1.0]
+    travel_times = np.array(
+        [[0, 5, 10, 15, 20], [5, 0, 5, 10, 15], [10, 5, 0, 5, 10], [15, 10, 5, 0, 5]]
+    )
+    beta = objective.get_beta(travel_times)
+
+    all_same_busy_1 = objective.get_all_same_closer_busy_vector(
+        utilisations, allocation_1, beta
+    )
+    all_same_busy_2 = objective.get_all_same_closer_busy_vector(
+        utilisations, allocation_2, beta
+    )
+    all_same_busy_3 = objective.get_all_same_closer_busy_vector(
+        utilisations, allocation_3, beta
+    )
+    expected_all_same_busy_1 = np.array(
+        [
+            [
+                np.prod(
+                    [
+                        utilisations[alpha] ** (allocation_1[alpha] * beta[p][alpha][a])
+                        for alpha in range(4)
+                    ]
+                )
+                for p in range(5)
+            ]
+            for a in range(4)
+        ]
+    )
+    expected_all_same_busy_2 = np.array(
+        [
+            [
+                np.prod(
+                    [
+                        utilisations[alpha] ** (allocation_2[alpha] * beta[p][alpha][a])
+                        for alpha in range(4)
+                    ]
+                )
+                for p in range(5)
+            ]
+            for a in range(4)
+        ]
+    )
+    expected_all_same_busy_3 = np.array(
+        [
+            [
+                np.prod(
+                    [
+                        utilisations[alpha] ** (allocation_3[alpha] * beta[p][alpha][a])
+                        for alpha in range(4)
+                    ]
+                )
+                for p in range(5)
+            ]
+            for a in range(4)
+        ]
+    )
+    assert np.allclose(all_same_busy_1, expected_all_same_busy_1)
+    assert np.allclose(all_same_busy_2, expected_all_same_busy_2)
+    assert np.allclose(all_same_busy_3, expected_all_same_busy_3)
+
+
+def test_get_all_primary_closer_busy_vector():
+    allocation_1 = [0, 0, 0, 0]
+    allocation_2 = [0, 1, 1, 1]
+    allocation_3 = [1, 2, 3, 4]
+    utilisations = [0.2, 0.5, 0.7, 1.0]
+    travel_times = np.array(
+        [[0, 5, 10, 15, 20], [5, 0, 5, 10, 15], [10, 5, 0, 5, 10], [15, 10, 5, 0, 5]]
+    )
+    R = objective.get_R(travel_times, travel_times * 0.7)
+
+    all_primary_closer_busy_1 = objective.get_all_primary_closer_busy_vector(
+        utilisations, allocation_1, R
+    )
+    all_primary_closer_busy_2 = objective.get_all_primary_closer_busy_vector(
+        utilisations, allocation_2, R
+    )
+    all_primary_closer_busy_3 = objective.get_all_primary_closer_busy_vector(
+        utilisations, allocation_3, R
+    )
+    expected_all_primary_closer_busy_1 = np.array(
+        [
+            [
+                np.prod(
+                    [
+                        utilisations[alpha] ** (allocation_1[alpha] * R[p][alpha][a])
+                        for alpha in range(4)
+                    ]
+                )
+                for a in range(4)
+            ]
+            for p in range(5)
+        ]
+    )
+    expected_all_primary_closer_busy_2 = np.array(
+        [
+            [
+                np.prod(
+                    [
+                        utilisations[alpha] ** (allocation_2[alpha] * R[p][alpha][a])
+                        for alpha in range(4)
+                    ]
+                )
+                for a in range(4)
+            ]
+            for p in range(5)
+        ]
+    )
+    expected_all_primary_closer_busy_3 = np.array(
+        [
+            [
+                np.prod(
+                    [
+                        utilisations[alpha] ** (allocation_3[alpha] * R[p][alpha][a])
+                        for alpha in range(4)
+                    ]
+                )
+                for a in range(4)
+            ]
+            for p in range(5)
+        ]
+    )
+    assert np.allclose(all_primary_closer_busy_1, expected_all_primary_closer_busy_1)
+    assert np.allclose(all_primary_closer_busy_2, expected_all_primary_closer_busy_2)
+    assert np.allclose(all_primary_closer_busy_3, expected_all_primary_closer_busy_3)
+
+
+def test_get_all_secondary_closer_busy_vector():
+    allocation_1 = [0, 0, 0, 0]
+    allocation_2 = [0, 1, 1, 1]
+    allocation_3 = [1, 2, 3, 4]
+    utilisations = [0.2, 0.5, 0.7, 1.0]
+    travel_times = np.array(
+        [[0, 5, 10, 15, 20], [5, 0, 5, 10, 15], [10, 5, 0, 5, 10], [15, 10, 5, 0, 5]]
+    )
+    R = objective.get_R(travel_times, travel_times * 0.7)
+
+    all_secondary_closer_busy_1 = objective.get_all_secondary_closer_busy_vector(
+        utilisations, allocation_1, R
+    )
+    all_secondary_closer_busy_2 = objective.get_all_secondary_closer_busy_vector(
+        utilisations, allocation_2, R
+    )
+    all_secondary_closer_busy_3 = objective.get_all_secondary_closer_busy_vector(
+        utilisations, allocation_3, R
+    )
+    expected_all_secondary_closer_busy_1 = np.array(
+        [
+            [
+                np.prod(
+                    [
+                        utilisations[alpha]
+                        ** (allocation_1[alpha] * (1 - R[p][a][alpha]))
+                        for alpha in range(4)
+                    ]
+                )
+                for a in range(4)
+            ]
+            for p in range(5)
+        ]
+    )
+    expected_all_secondary_closer_busy_2 = np.array(
+        [
+            [
+                np.prod(
+                    [
+                        utilisations[alpha]
+                        ** (allocation_2[alpha] * (1 - R[p][a][alpha]))
+                        for alpha in range(4)
+                    ]
+                )
+                for a in range(4)
+            ]
+            for p in range(5)
+        ]
+    )
+    expected_all_secondary_closer_busy_3 = np.array(
+        [
+            [
+                np.prod(
+                    [
+                        utilisations[alpha]
+                        ** (allocation_3[alpha] * (1 - R[p][a][alpha]))
+                        for alpha in range(4)
+                    ]
+                )
+                for a in range(4)
+            ]
+            for p in range(5)
+        ]
+    )
+    assert np.allclose(
+        all_secondary_closer_busy_1, expected_all_secondary_closer_busy_1
+    )
+    assert np.allclose(
+        all_secondary_closer_busy_2, expected_all_secondary_closer_busy_2
+    )
+    assert np.allclose(
+        all_secondary_closer_busy_3, expected_all_secondary_closer_busy_3
+    )
+
+
+def test_get_objective():
+    primary_travel_times = np.array(
+        [[0, 5, 10, 15, 20], [5, 0, 5, 10, 15], [10, 5, 0, 5, 10], [15, 10, 5, 0, 5]]
+    )
+    secondary_travel_times = 0.7 * primary_travel_times
+    beta = objective.get_beta(primary_travel_times)
+    R = objective.get_R(primary_travel_times, secondary_travel_times)
+    survival_functions = (
+        lambda t: np.ones(t.shape),
+        lambda t: np.ones(t.shape),
+        lambda t: np.ones(t.shape),
+    )
+    primary_survivals, secondary_survivals = objective.get_survival_time_vectors(
+        survival_functions, primary_travel_times, secondary_travel_times
+    )
+    given_utilisations_primary = np.array([0.2, 0.5, 0.7, 1.0])
+    given_utilisations_secondary = np.array([0.6, 0.6, 0.2, 0.2])
+    demand_rates = np.array(((2, 2, 3, 3, 7), (2, 0, 1, 2, 4), (1, 1, 1, 1, 1))) * 10
+
+    # Some allocation
+    g = objective.get_objective(
+        demand_rates=demand_rates,
+        primary_survivals=primary_survivals,
+        secondary_survivals=secondary_survivals,
+        weights_single_vehicle=np.array([0, 0, 1]),
+        weights_multiple_vehicles=np.array([1, 1, 0]),
+        beta=beta,
+        R=R,
+        vehicle_station_utilisation_function=utilisation.given_utilisations,
+        allocation_primary=np.array([1, 0, 0, 1]),
+        allocation_secondary=np.array([0, 2, 1, 1]),
+        given_utilisations_primary=given_utilisations_primary,
+        given_utilisations_secondary=given_utilisations_secondary,
+    )
+    assert round(g, 4) == 295.1552
+
+    # Zero allocation (cannot save anyone)
+    g = objective.get_objective(
+        demand_rates=demand_rates,
+        primary_survivals=primary_survivals,
+        secondary_survivals=secondary_survivals,
+        weights_single_vehicle=np.array([0, 0, 1]),
+        weights_multiple_vehicles=np.array([1, 1, 0]),
+        beta=beta,
+        R=R,
+        vehicle_station_utilisation_function=utilisation.given_utilisations,
+        allocation_primary=np.array([0, 0, 0, 0]),
+        allocation_secondary=np.array([0, 0, 0, 0]),
+        given_utilisations_primary=given_utilisations_primary,
+        given_utilisations_secondary=given_utilisations_secondary,
+    )
+    assert round(g, 4) == 0.0
+
+    # Over allocation (always saving everyone)
+    g = objective.get_objective(
+        demand_rates=demand_rates,
+        primary_survivals=primary_survivals,
+        secondary_survivals=secondary_survivals,
+        weights_single_vehicle=np.array([0, 0, 1]),
+        weights_multiple_vehicles=np.array([1, 1, 0]),
+        beta=beta,
+        R=R,
+        vehicle_station_utilisation_function=utilisation.given_utilisations,
+        allocation_primary=np.array([1000, 1000, 1000, 1000]),
+        allocation_secondary=np.array([1000, 1000, 1000, 1000]),
+        given_utilisations_primary=given_utilisations_primary,
+        given_utilisations_secondary=given_utilisations_secondary,
+    )
+    assert round(g, 4) == demand_rates.sum()
